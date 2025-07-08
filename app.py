098d90efc3e4b31d501489d1d743909c7fc60c1e
unsugarcoffe <zuanxzy@gmail.com>
2025-07-08 20:02:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,6 @@ def local_css(file_name):
 
 local_css("style/style.css") 
 
-# ===== CSS =====
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("style/style.css")
-
 # ===== ASSETS =====
 def load_lottieurl(url):
     try:
